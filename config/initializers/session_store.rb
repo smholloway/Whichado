ae3017c139f318c29cbe29230b28a9486b327abf
Shayne Czyzewski <thelastemail@gmail.com>
2011-05-06 14:01:42 -0500

    renamed to whichado

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MongoidTest::Application.config.session_store :cookie_store, :key => '_mongoid_test_session'
+MongoidTest::Application.config.session_store :cookie_store, :key => '_whichado_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
