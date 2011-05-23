02457d2b84116d5ed1a5b5a0dfb57f3c2fcdb02c
Shayne Czyzewski <thelastemail@gmail.com>
2011-05-23 17:49:17 -0500

    fixed default title display for rails 3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+  
+  def yield_for(content_sym, default)
+    output = content_for(content_sym)
+    output = default if output.blank?
+    output
+  end
 end
